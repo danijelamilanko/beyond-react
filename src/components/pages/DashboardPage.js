7f81819940b9c316613feb108f2f4f910f356e88
Danijela Milanko <ddmilanko@gmail.com>
2017-12-09 10:14:31 +0100

    hoc..

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -3,12 +3,19 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { allItemsSelector } from '../../reducers/items';
 import AddItemCta from '../ctas/AddItemCta'
+import LoadingHOC from '../HOC/LoadingHOC'
 
-const DashboardPage = ({items}) => (
-    <div>
-        {items.length === 0 && <AddItemCta/>}
-    </div>
-);
+class DashboardPage extends React.Component {
+
+    render() {
+        const {items} = this.props;
+        return (
+            <div>
+                {items.length === 0 ? <AddItemCta /> : <p>You have books!</p>}
+            </div>
+        );
+    }
+}
 
 DashboardPage.propTypes = {
     items: PropTypes.arrayOf(PropTypes.shape({
